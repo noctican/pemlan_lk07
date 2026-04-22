21c4666705a842cc164dc95e9e6957ce7895da67
Haidar N. <ub.haidarnadhifathaputra@gmail.com>
2026-04-22 09:30:08 +0700

    Inisialisasi awal

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+package src;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    // Atribut
+    private static ArrayList<String> data = new ArrayList<String>();
+    private final static File lokasiFile = new File("./src/data/dataSiswa.txt");
+    private final static File dirFile = new File("./src/data");
+
+    // Method
+    public static void save(){
+        if(!dirFile.exists()) {dirFile.mkdir();}
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(lokasiFile))){
+            for(int i=0; i<data.size(); i++){
+                bw.write(data.get(i));
+
+                if(i != data.size()-1){
+                    bw.newLine();
+                }
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void load(){
+        if(!dirFile.exists()) {dirFile.mkdir();}
+
+        if(!lokasiFile.exists()){ return; }
+
+        try(BufferedReader br = new BufferedReader(new FileReader(lokasiFile))){
+            String line;
+
+            while((line = br.readLine()) != null){
+                data.add(line);
+            }
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
+}
