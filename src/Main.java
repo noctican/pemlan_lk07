a80ba4ab47552f7ce53ac33e3a79bd4304d50c50
Haidar N. <ub.haidarnadhifathaputra@gmail.com>
2026-04-22 10:03:31 +0700

    Fix

--- src/Main.java
+++ src/Main.java
@@ -11,43 +11,45 @@ import java.io.IOException;
 public class Main {
     // Atribut
     private static ArrayList<String> data = new ArrayList<String>();
-    private final static File lokasiFile = new File("./src/data/dataSiswa.txt");
+    private final static File lokasiFile = new File("./src/data/dataSiswa.csv");
     private final static File dirFile = new File("./src/data");
 
     // Method
-    public static void save(){
-        if(!dirFile.exists()) {dirFile.mkdir();}
+    public static void save() {
+        if (!dirFile.exists()) dirFile.mkdir();
 
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(lokasiFile))){
-            for(int i=0; i<data.size(); i++){
-                bw.write(data.get(i));
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(lokasiFile))) {
 
-                if(i != data.size()-1){
+            for (int i = 0; i < data.size(); i++) {
+                String[] pisah = data.get(i).trim().split(";");
+                bw.write(String.join(",", pisah));
+
+                if (i != data.size() - 1) {
                     bw.newLine();
                 }
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void load(){
-        if(!dirFile.exists()) {dirFile.mkdir();}
+    public static void load() {
+        if (!dirFile.exists()) dirFile.mkdir();
+        if (!lokasiFile.exists()) return;
 
-        if(!lokasiFile.exists()){ return; }
-
-        try(BufferedReader br = new BufferedReader(new FileReader(lokasiFile))){
+        try (BufferedReader br = new BufferedReader(new FileReader(lokasiFile))) {
             String line;
 
-            while((line = br.readLine()) != null){
-                data.add(line);
+            while ((line = br.readLine()) != null) {
+                String[] split = line.trim().split(",");
+                data.add(String.join(";", split));
             }
-        } catch (IOException e){
+
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
     public static void main(String[] args){
-
     }
 }
