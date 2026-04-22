a189a18dec8dab6711fb4ba26caaff2702509d40
NovalZakky <novalzakky@student.ub.ac.id>
2026-04-22 10:52:41 +0700

    Menambah exception

---
+++ src/exceptions/DataNotFoundException.java
@@ -0,0 +1,5 @@
+package src.exceptions;
+
+public class DataNotFoundException extends Exception {
+    public DataNotFoundException(String message) { super(message); }
+}
---
+++ src/exceptions/DuplicateDataException.java
@@ -0,0 +1,5 @@
+package src.exceptions;
+
+public class DuplicateDataException extends Exception {
+    public DuplicateDataException(String message) { super(message); }
+}
